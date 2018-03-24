d78dce92073fec66dc462f35691484f8a9383e16
Michael Freeman <michael.k.freeman@gmail.com>
2018-03-23 21:32:49 -0700

    Add starter files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# a1-starter
-Starter Repo for Assignment 1
+# a1-news
+
+In this assignment, you'll edit this `README.md` file to create a Markdown document that describes a news story (full instructions on Canvas). You'll also keep track of the terminal commands you use in the `session.sh` file (you don't ever need to _run_ that file -- it's just to show us _how_ you can use terminal commands).
+
+When you're finished, please **delete this text**, leaving only the Markdown that you've written. Make sure to **submit this GitHub URL** to Canvas on time for full credit.
---
+++ session.sh
@@ -0,0 +1,31 @@
+# session.sh
+# Use this file to track the commands that you execute in your terminal
+# You don't need to *run* this file, just list out the appropriate command after each prompt
+
+# Print your working directory
+
+
+# Change your directory to a folder in which you do work for this class
+# Make sure to use the "~" shortcut rather than specifying the full path
+
+
+# Clone your (private) assignment repository from GitHub to your machine
+
+
+# Change your directory to inside of your "a1-news-USERNAME" folder
+
+
+# Make a new folder called "imgs" - you'll download an image into this folder
+
+
+# At appropriate checkpoints, you'll need to do the following version control steps
+# (feel free to only list these steps once)
+
+# Add all of your changes that you've made to git
+
+
+# Make a commit, including a *descriptive message*
+
+
+# Push your change up to GitHub
+
